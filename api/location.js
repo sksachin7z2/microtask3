c1324db9f65ec511441be65d76276bf6358724e3
sachin <sksachin7z2@gmail.com>
2023-01-16 23:06:38 +0530

    some fixes

--- api/location.js
+++ api/location.js
@@ -17,6 +17,23 @@ router.get('/getAdd',async(req,res)=>{
         res.status(500).send("Internal Server Error");
     }
 })
+router.get('/getCoor',async(req,res)=>{
+    const {country,state,district,city,locality}=req.query;
+    try {
+        const url=`https://nominatim.openstreetmap.org/?q=${country}+${state}+${district}+${city}+${locality}&format=jsonv2&limit=1&addressdetails=1`
+        const resp=await axios.get(url);
+        const data=resp.data;
+        if(!data)
+        return res.status(400).json({status:"Incorrect Format"})
+
+
+        res.json({status:"Coordinates",latitude:data[0].lat,longitude:data[0].lon,coordinates:data});
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Internal Server Error");
+    }
+})
 
 
 module.exports=router;
